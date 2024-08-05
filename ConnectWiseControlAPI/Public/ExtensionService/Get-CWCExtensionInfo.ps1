b6abaece440b5e2508772ee6cc08a98df712b1c4
Scott Linak <scott@linak.org>
2024-08-05 15:59:40 -0700

    added getextensioninfo
    returns installed extensions and their state

---
+++ ConnectWiseControlAPI/Public/ExtensionService/Get-CWCExtensionInfo.ps1
@@ -0,0 +1,13 @@
+﻿function Get-CWCExtensionInfo {
+    [CmdletBinding()]
+    param ()
+
+    $Endpoint = 'Services/ExtensionService.ashx/GetExtensionInfos'
+
+    $WebRequestArguments = @{
+        Endpoint = $Endpoint
+        Method   = 'Post'
+    }
+
+    Invoke-CWCWebRequest -Arguments $WebRequestArguments
+}
